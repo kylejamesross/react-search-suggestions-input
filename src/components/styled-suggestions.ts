a3601f6b9a2c1ddee6544619a8ceadadbce658c4
Kyle Ross <kylejamesross@gmail.com>
2023-11-11 22:39:22 -0700

    Add theme default scrollbar for suggestions

--- src/components/styled-suggestions.ts
+++ src/components/styled-suggestions.ts
@@ -11,5 +11,21 @@ export const StyledSuggestions = styled.div`
     opacity: 0;
     visiblity: hidden;
     transition: opacity 0.3s ease-in-out;
-`;
 
+    &::-webkit-scrollbar-track {
+        -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.2);
+        border-radius: 10px;
+        background-color: #f5f5f5;
+    }
+
+    &::-webkit-scrollbar {
+        width: 12px;
+        background-color: #f5f5f5;
+    }
+
+    &::-webkit-scrollbar-thumb {
+        border-radius: 10px;
+        -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.2);
+        background-color: #aaa;
+    }
+`;
